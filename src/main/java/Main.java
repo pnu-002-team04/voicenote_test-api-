1156a74afe89d9bc4683df877ecf4bb97b17709c
audwlsd <audwlsd7782@naver.com>
2019-04-05 10:20:11 +0900

    API 빠른 시작 코드 작성하기
    #VOEA-169

---
+++ src/main/java/Main.java
@@ -0,0 +1,71 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import com.google.cloud.speech.v1p1beta1.RecognitionAudio;
+import com.google.cloud.speech.v1p1beta1.RecognitionConfig;
+import com.google.cloud.speech.v1p1beta1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1p1beta1.RecognizeResponse;
+import com.google.cloud.speech.v1p1beta1.SpeechClient;
+import com.google.cloud.speech.v1p1beta1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1p1beta1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+        try (SpeechClient speechClient = SpeechClient.create()) {
+
+            // The path to the audio file to transcribe
+//            File f =new File("C:\\Users\\audwl\\Documents\\NetBeansProjects\\MyJAVAFX\\src\\main\\resources\\11.wav");
+//            String fileName = f.getName();
+//            System.out.println(f.getName());
+
+            // Reads the audio file into memory
+            Path path = Paths.get("C:\\Users\\audwl\\Desktop\\example.wav");
+            byte[] data = Files.readAllBytes(path);
+            ByteString audioBytes = ByteString.copyFrom(data);
+
+            // Builds the sync recognize request
+            RecognitionConfig config = RecognitionConfig.newBuilder()
+                    .setEncoding(AudioEncoding.LINEAR16)
+                    .setSampleRateHertz(44100)
+                    .setLanguageCode("ko-kr")
+                    .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder()
+                    .setContent(audioBytes)
+                    .build();
+
+            // Performs speech recognition on the audio file
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+
+            for (SpeechRecognitionResult result : results) {
+                // There can be several alternative transcripts for a given chunk of speech. Just use the
+                // first (most likely) one here.
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            }
+        }catch (Exception e){
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
+
